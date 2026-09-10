a679affe8ab338a0d9d5c9a253308194cb53e5ac
BaconEggCheese777 <hikarureborn@gmail.com>
2026-09-10 17:07:20 -0400

    Update OrganTraumaDefOf.cs

--- OrganTrauma/Source/OrganTraumaMod/OrganTraumaDefOf.cs
+++ OrganTrauma/Source/OrganTraumaMod/OrganTraumaDefOf.cs
@@ -7,8 +7,6 @@ namespace OrganTraumaMod
     public static class OrganTraumaDefOf
     {
         public static HediffDef OrganTrauma_ManipulationPenalty;
-        public static HediffDef OrganTrauma_BrainConsciousnessPenalty;
-
         static OrganTraumaDefOf()
         {
             DefOfHelper.EnsureInitializedInCtor(typeof(OrganTraumaDefOf));
